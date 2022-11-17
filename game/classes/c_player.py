cdbb9f087d6f0852b0bba2c521a3f44e345bf234
Th3R3ality <63677818+Th3R3ality@users.noreply.github.com>
2022-11-17 19:31:51 +0100

    movement

--- game/classes/c_player.py
+++ game/classes/c_player.py
@@ -4,17 +4,37 @@ class Player:
 
     def __init__(self):
         #sprites n shit
-        self.surf = pygame.Surface((30, 30))
-        self.surf.fill((128,255,40))
-
-
+        #self.surf = pygame.Surface((64, 64))
+        #self.surf.fill(("#000000"))
+        #self.rect = self.surf.get_rect(center = (400, 300))
 
         #mechanical shit
         self.health = 100
         self.movement_Speed = 1
-
+        
         #movement shit
+        self.x = 400
+        self.y = 300
         self.moving_right = False
         self.moving_left = False
         self.moving_up = False
         self.moving_down = False
+    def update(self, surface, time):
+        #draw the player
+        pygame.draw.rect(surface, (244, 0, 0), (self.x, self.y, 64, 64))
+
+
+        #movement
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.y -= self.movement_Speed * time
+        if keys[pygame.K_s]:
+            self.y += self.movement_Speed * time
+        if keys[pygame.K_a]:
+            self.x -= self.movement_Speed * time
+        if keys[pygame.K_d]:
+            self.x += self.movement_Speed * time
+
+                    
+
+print("Player initialized")
